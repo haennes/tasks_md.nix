872af3995dd9e4defa420805fa06506387c42c31
haennes <hannes.hofmuth@gmail.com>
2024-05-21 22:39:57 +0200

    move to foreach

---
+++ foreach.nix
@@ -0,0 +1,32 @@
+{lib, item}:
+let
+    cont_name = item: builtins.replaceStrings ["/"]["_"] item.tasks_dir;
+in
+{
+      virtualisation.oci-containers.containers."tasks.md${cont_name item}" = {
+        image = "baldissaramatheus/tasks.md";
+        environment = {
+          PGID = toString item.guid;
+          PUID = toString item.puid;
+          TITLE = item.title;
+          BASE_PATH = item.base_path;
+        };
+        volumes = [ "${item.config_dir}:/config:rw" "${item.tasks_dir}:/tasks:rw" ];
+        ports = [ "${toString item.port}:8080/tcp" ];
+        log-driver = "journald";
+        extraOptions =
+          [ "--network-alias=tasks.md${cont_name item}" "--network=tasksmd_default" ];
+      };
+      systemd.services."docker-tasks.md${cont_name item}" = {
+        serviceConfig = {
+          Restart = lib.mkOverride 500 "always";
+          RestartMaxDelaySec = lib.mkOverride 500 "1m";
+          RestartSec = lib.mkOverride 500 "100ms";
+          RestartSteps = lib.mkOverride 500 9;
+        };
+        after = [ "docker-network-tasksmd_default.service" ];
+        requires = [ "docker-network-tasksmd_default.service" ];
+        partOf = [ "docker-compose-tasksmd-root.target" ];
+        wantedBy = [ "docker-compose-tasksmd-root.target" ];
+      };
+    }
--- tasks.nix
+++ tasks.nix
@@ -89,41 +89,9 @@ in {
   };
   config = lib.mkIf cfg.enable (
   recursiveMerge ([(import ./base.nix{inherit pkgs;})] ++
-  #[(
-  #let
-  #item = lib.head cfg.conf;
-  #in
-  lib.lists.map(item:
-  {
-      virtualisation.oci-containers.containers."tasks.md${cont_name item}" = {
-        image = "baldissaramatheus/tasks.md";
-        environment = {
-          PGID = toString item.guid;
-          PUID = toString item.puid;
-          TITLE = item.title;
-          BASE_PATH = item.base_path;
-        };
-        volumes = [ "${item.config_dir}:/config:rw" "${item.tasks_dir}:/tasks:rw" ];
-        ports = [ "${toString item.port}:8080/tcp" ];
-        log-driver = "journald";
-        extraOptions =
-          [ "--network-alias=tasks.md${cont_name item}" "--network=tasksmd_default" ];
-      };
-      systemd.services."docker-tasks.md${cont_name item}" = {
-        serviceConfig = {
-          Restart = lib.mkOverride 500 "always";
-          RestartMaxDelaySec = lib.mkOverride 500 "1m";
-          RestartSec = lib.mkOverride 500 "100ms";
-          RestartSteps = lib.mkOverride 500 9;
-        };
-        after = [ "docker-network-tasksmd_default.service" ];
-        requires = [ "docker-network-tasksmd_default.service" ];
-        partOf = [ "docker-compose-tasksmd-root.target" ];
-        wantedBy = [ "docker-compose-tasksmd-root.target" ];
-      };
-    }
-    ) cfg.conf #uncomment if map
-    )
-    #])
+  [  #lib.lists.map(item:
+  (import ./foreach.nix{inherit lib; item = (lib.elemAt cfg.conf 0);})
+    #) cfg.conf #uncomment if map
+  ])
     );
 }
