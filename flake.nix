28dc09660394e9868b7ced73c6464cca161a8524
haennes <hannes.hofmuth@gmail.com>
2024-10-28 15:54:01 +0100

    move example to toplevel flake

--- flake.nix
+++ flake.nix
@@ -5,11 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
     nixosModules = rec {
       default = tasks;
       tasks = import ./tasks.nix;
       home-manager = import ./tasks.nix;
+    };
+    nixosConfigurations.host1 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./example.nix nixosModules.default ];
     };
   };
 }
