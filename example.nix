99e4cd912471d6142516601e33c1850344323477
haennes <hannes.hofmuth@gmail.com>
2024-10-31 15:57:42 +0100

    fix example on this branch

--- example.nix
+++ example.nix
@@ -1,9 +1,26 @@
-{...}:{
-services.tasks_md = {
-  enable = true;
-  conf = {
-    config_dir = "/home/hannses/tasks/config";
-    tasks_dir = "/home/hannses/tasks/tasks";
+{ ... }: {
+  services.tasks_md = {
+    enable = true;
+    conf = [
+      {
+        config_dir = "/home/hannses/tasks1/config";
+        tasks_dir = "/home/hannses/tasks1/tasks";
+      }
+
+      {
+        config_dir = "/home/hannses/tasks2/config";
+        tasks_dir = "/home/hannses/tasks2/tasks";
+      }
+
+      {
+        config_dir = "/home/hannses/tasks3/config";
+        tasks_dir = "/home/hannses/tasks3/tasks";
+      }
+
+      {
+        config_dir = "/home/hannses/tasks4/config";
+        tasks_dir = "/home/hannses/tasks4/tasks";
+      }
+    ];
   };
-};
 }
