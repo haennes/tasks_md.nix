5a582aeebd7e336fc0a433d71c045a5d92054174
haennes <hannes.hofmuth@gmail.com>
2024-10-31 16:06:22 +0100

    fmt

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }: rec {
     nixosModules = rec {
@@ -15,5 +13,8 @@
       system = "x86_64-linux";
       modules = [ ./example.nix nixosModules.default ];
     };
+    formatter = let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+    in forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-classic);
   };
 }
--- foreach.nix
+++ foreach.nix
@@ -1,39 +1,39 @@
-{lib, item}:
-let
-    cont_name = item: builtins.replaceStrings ["/"]["_"] item.tasks_dir;
-in
-{
-      virtualisation.oci-containers.containers."tasks.md${cont_name item}" = {
-        image = "baldissaramatheus/tasks.md:2.5.3";
-        environment = {
-          PGID = toString item.guid;
-          PUID = toString item.puid;
-          TITLE = item.title;
-          BASE_PATH = item.base_path;
-        };
-        volumes = [ "${item.config_dir}:/config:rw" "${item.tasks_dir}:/tasks:rw" ];
-        ports = [ "${toString item.port}:8080/tcp" ];
-        log-driver = "journald";
-        extraOptions =
-          [ "--network-alias=tasks.md${cont_name item}" "--network=tasksmd_default" ];
-      };
-      systemd.services."docker-tasks.md${cont_name item}" = {
-        serviceConfig = {
-          Restart = lib.mkOverride 500 "always";
-          RestartMaxDelaySec = lib.mkOverride 500 "1m";
-          RestartSec = lib.mkOverride 500 "100ms";
-          RestartSteps = lib.mkOverride 500 9;
-        };
-        after = [ "docker-network-tasksmd_default.service" ];
-        requires = [ "docker-network-tasksmd_default.service" ];
-        partOf = [ "docker-compose-tasksmd-root.target" ];
-        wantedBy = [ "docker-compose-tasksmd-root.target" ];
-      };
-    services.nginx.virtualHosts = {
-      "${item.domain}" = {
-        locations."${item.base_path}" = {
-          proxyPass = "http://localhost:${toString item.port}${item.base_path}";
-        };
+{ lib, item }:
+let cont_name = item: builtins.replaceStrings [ "/" ] [ "_" ] item.tasks_dir;
+in {
+  virtualisation.oci-containers.containers."tasks.md${cont_name item}" = {
+    image = "baldissaramatheus/tasks.md:2.5.3";
+    environment = {
+      PGID = toString item.guid;
+      PUID = toString item.puid;
+      TITLE = item.title;
+      BASE_PATH = item.base_path;
+    };
+    volumes = [ "${item.config_dir}:/config:rw" "${item.tasks_dir}:/tasks:rw" ];
+    ports = [ "${toString item.port}:8080/tcp" ];
+    log-driver = "journald";
+    extraOptions = [
+      "--network-alias=tasks.md${cont_name item}"
+      "--network=tasksmd_default"
+    ];
+  };
+  systemd.services."docker-tasks.md${cont_name item}" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
+    };
+    after = [ "docker-network-tasksmd_default.service" ];
+    requires = [ "docker-network-tasksmd_default.service" ];
+    partOf = [ "docker-compose-tasksmd-root.target" ];
+    wantedBy = [ "docker-compose-tasksmd-root.target" ];
+  };
+  services.nginx.virtualHosts = {
+    "${item.domain}" = {
+      locations."${item.base_path}" = {
+        proxyPass = "http://localhost:${toString item.port}${item.base_path}";
       };
     };
-    }
+  };
+}
